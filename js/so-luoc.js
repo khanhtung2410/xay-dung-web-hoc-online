84468dc2eb88fe6efd4e5717d0bb841a122f91cf
khanhtung2410 <khanhtungdaoduy2410@gmail.com>
2023-09-19 15:14:29 +0700

    19/09

--- js/so-luoc.js
+++ js/so-luoc.js
@@ -4,4 +4,13 @@ function menudropdown(){
     x.className += " w3-show";
    else 
     x.className = x.className.replace(" w3-show", "");
-}                        
+} 
+function login(){
+  document.getElementById("change-form").textContent = "Log in";
+  document.getElementById("status").innerHTML = "SIGN UP";
+  document.getElementById("confirm").value = "Sign up for free";
+  document.getElementById("acc-Q").innerHTML = "Already have acount ?"
+  document.getElementById("pass-validation").style.display="block";
+  document.getElementById("email").style.display="block";
+  document.getElementById("remember").style.display="none";
+}                       
